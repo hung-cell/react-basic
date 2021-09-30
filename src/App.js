69ab352b01d3fdf821b3e328d4a08e091045a60c
hung-cell <daoquanghung922@gmail.com>
2021-09-30 19:28:33 +0700

    init-project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tao phải dành 4 năm nữa để mà chán ghét
         </p>
         <a
           className="App-link"
